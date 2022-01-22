d9d4238342e5e5173671f88d5cfebb44ae250038
rezaabdullah <air.reza@hotmail.com>
2022-01-22 09:55:37 +0600

    feat: added IBM Plex Sans

--- gatsby-config.js
+++ gatsby-config.js
@@ -43,11 +43,10 @@ module.exports = {
       },
     },
     {
-      resolve: `gatsby-plugin-google-fonts`,
+      resolve: "gatsby-plugin-google-fonts",
       options: {
         fonts: [
-          `limelight`,
-          `source sans pro\:300,400,400i,700` // you can also specify font weights and styles
+          "ibm plex sans",
         ],
         display: 'swap'
       }
